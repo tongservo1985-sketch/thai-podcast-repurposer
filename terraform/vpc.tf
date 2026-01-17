7015481fa37215dcdeb021e7c54e8c6f42d54a3d
tongservo1985-sketch <tongservo1985@gmail.com>
2026-01-17 11:57:12 +0700

    SWARM_SYNC: Update terraform/vpc.tf

---
+++ terraform/vpc.tf
@@ -0,0 +1,17 @@
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.1.0"
+
+  name = "${var.project_name}-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = slice(data.aws_availability_zones.available.names, 0, 2)
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true # Cost-efficient for start-up phase
+
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+}
