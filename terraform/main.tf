87230e50e423c8b0b88b69e82c0cad3c319309f6
tongservo1985-sketch <tongservo1985@gmail.com>
2026-01-17 11:57:11 +0700

    SWARM_SYNC: Update terraform/main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  region = var.aws_region
+
+  default_tags {
+    tags = {
+      Project     = var.project_name
+      Environment = var.environment
+      ManagedBy   = "Terraform"
+    }
+  }
+}
+
+# Data source for AZs
+data "aws_availability_zones" "available" {}
