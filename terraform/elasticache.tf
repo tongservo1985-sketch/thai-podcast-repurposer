74c7c905e77f501bd87afa5d8a52d6ebdd18b393
tongservo1985-sketch <tongservo1985@gmail.com>
2026-01-17 11:57:17 +0700

    SWARM_SYNC: Update terraform/elasticache.tf

---
+++ terraform/elasticache.tf
@@ -0,0 +1,14 @@
+resource "aws_elasticache_subnet_group" "redis" {
+  name       = "${var.project_name}-redis-subnet"
+  subnet_ids = module.vpc.private_subnets
+}
+
+resource "aws_elasticache_cluster" "celery_broker" {
+  cluster_id           = "${var.project_name}-redis"
+  engine               = "redis"
+  node_type            = "cache.t3.micro"
+  num_cache_nodes      = 1
+  parameter_group_name = "default.redis7"
+  port                 = 6379
+  subnet_group_name    = aws_elasticache_subnet_group.redis.name
+}
